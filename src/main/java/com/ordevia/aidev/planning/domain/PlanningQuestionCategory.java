ff305813052105431433a22bac00bdf12a9c8e26
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 17:24:55 -0300

    feat: add planning question categories

---
+++ src/main/java/com/ordevia/aidev/planning/domain/PlanningQuestionCategory.java
@@ -0,0 +1,12 @@
+package com.ordevia.aidev.planning.domain;
+
+public enum PlanningQuestionCategory {
+    BUSINESS_RULE,
+    SCOPE,
+    USER_EXPERIENCE,
+    DATA,
+    INTEGRATION,
+    SECURITY,
+    OPERATIONS,
+    OTHER
+}
