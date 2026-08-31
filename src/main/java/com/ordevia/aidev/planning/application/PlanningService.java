18d76efc9cc0cec33a93a39d7a1628bbd7cbff11
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 17:39:19 -0300

    feat: validate domain after planning approval

--- src/main/java/com/ordevia/aidev/planning/application/PlanningService.java
+++ src/main/java/com/ordevia/aidev/planning/application/PlanningService.java
@@ -99,7 +99,7 @@ public class PlanningService {
         }
         session.approve();
         item.setSpecification(session.getFinalSpecification());
-        item.moveTo(WorkItemStatus.READY_FOR_ARCHITECTURE);
+        item.moveTo(WorkItemStatus.READY_FOR_DOMAIN_VALIDATION);
         sessions.save(session);
         workItems.save(item);
         return view(session);
