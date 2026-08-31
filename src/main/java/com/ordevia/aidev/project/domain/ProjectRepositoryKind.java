f62ac8fe543984252e13445646b1ab10ed5f7252
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 18:35:40 -0300

    feat: add project repository profiles

---
+++ src/main/java/com/ordevia/aidev/project/domain/ProjectRepositoryKind.java
@@ -0,0 +1,13 @@
+package com.ordevia.aidev.project.domain;
+
+public enum ProjectRepositoryKind {
+    LEGACY_BACKEND,
+    BACKEND,
+    FRONTEND,
+    FULLSTACK,
+    SHARED_LIBRARY,
+    INFRASTRUCTURE,
+    DATABASE,
+    DOCUMENTATION,
+    OTHER
+}
