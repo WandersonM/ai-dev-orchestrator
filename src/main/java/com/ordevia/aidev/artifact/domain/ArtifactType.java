3a1c564677ecd50654ac8ba2aa2f35f496d8af0d
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 20:26:04 -0300

    feat: define proof of work artifact types

---
+++ src/main/java/com/ordevia/aidev/artifact/domain/ArtifactType.java
@@ -0,0 +1,13 @@
+package com.ordevia.aidev.artifact.domain;
+
+public enum ArtifactType {
+    LOG,
+    TEST_REPORT,
+    COVERAGE_REPORT,
+    SCREENSHOT,
+    VIDEO,
+    CONTRACT_REPORT,
+    SECURITY_REPORT,
+    MIGRATION_REPORT,
+    OTHER
+}
