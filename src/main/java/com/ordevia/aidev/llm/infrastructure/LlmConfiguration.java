b9343077bf979ba75867720f5e656863bc93b445
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 13:22:49 -0300

    feat: add LLM configuration

---
+++ src/main/java/com/ordevia/aidev/llm/infrastructure/LlmConfiguration.java
@@ -0,0 +1,8 @@
+package com.ordevia.aidev.llm.infrastructure;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(LlmRoutingProperties.class)
+public class LlmConfiguration {}
