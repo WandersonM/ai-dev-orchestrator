96269748e687a142f73598f43ca05bba9dbb3281
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 18:36:05 -0300

    feat: model work item repository bindings

---
+++ src/main/java/com/ordevia/aidev/workitem/domain/WorkItemRepositoryPurpose.java
@@ -0,0 +1,12 @@
+package com.ordevia.aidev.workitem.domain;
+
+public enum WorkItemRepositoryPurpose {
+    PRIMARY,
+    BACKEND,
+    FRONTEND,
+    INTEGRATION,
+    TEST,
+    INFRASTRUCTURE,
+    SHARED,
+    OTHER
+}
