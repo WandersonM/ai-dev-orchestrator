84bc79f108074e60ba70777f7e5db38ed5d8ff4a
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 15:03:52 -0300

    feat: query work items by project

--- src/main/java/com/ordevia/aidev/workitem/infrastructure/WorkItemJpaRepository.java
+++ src/main/java/com/ordevia/aidev/workitem/infrastructure/WorkItemJpaRepository.java
@@ -2,6 +2,10 @@ package com.ordevia.aidev.workitem.infrastructure;
 
 import com.ordevia.aidev.workitem.domain.WorkItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.UUID;
 
-public interface WorkItemJpaRepository extends JpaRepository<WorkItem, UUID> {}
+public interface WorkItemJpaRepository extends JpaRepository<WorkItem, UUID> {
+    List<WorkItem> findByProjectIdOrderByCreatedAtAsc(UUID projectId);
+}
