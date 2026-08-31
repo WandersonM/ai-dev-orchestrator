769ee71dacbfcf0abdde0181b5a4bdcb5372a375
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 13:21:13 -0300

    feat: add application entrypoint

---
+++ src/main/java/com/ordevia/aidev/AiDevOrchestratorApplication.java
@@ -0,0 +1,11 @@
+package com.ordevia.aidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDevOrchestratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiDevOrchestratorApplication.class, args);
+    }
+}
