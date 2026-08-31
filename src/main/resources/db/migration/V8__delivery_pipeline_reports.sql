d21ded2be0c95d578ecb9eff43df3ab7ce50befb
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 17:37:59 -0300

    feat: persist full delivery pipeline reports

---
+++ src/main/resources/db/migration/V8__delivery_pipeline_reports.sql
@@ -0,0 +1,6 @@
+ALTER TABLE work_item ADD COLUMN domain_validation_report TEXT;
+ALTER TABLE work_item ADD COLUMN delivery_roles VARCHAR(200);
+ALTER TABLE work_item ADD COLUMN integration_report TEXT;
+ALTER TABLE work_item ADD COLUMN qa_report TEXT;
+ALTER TABLE work_item ADD COLUMN security_report TEXT;
+ALTER TABLE work_item ADD COLUMN release_report TEXT;
