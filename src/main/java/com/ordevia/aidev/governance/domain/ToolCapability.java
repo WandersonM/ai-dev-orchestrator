af5a3abd640d3ce045b9f42a530bd782b29f8191
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 19:43:41 -0300

    feat: add tool capabilities

---
+++ src/main/java/com/ordevia/aidev/governance/domain/ToolCapability.java
@@ -0,0 +1,14 @@
+package com.ordevia.aidev.governance.domain;
+
+public enum ToolCapability {
+    READ,
+    WRITE,
+    EXECUTE,
+    NETWORK,
+    DATABASE_READ,
+    DATABASE_WRITE,
+    GIT_READ,
+    GIT_WRITE,
+    EXTERNAL_MUTATION,
+    PRODUCTION
+}
