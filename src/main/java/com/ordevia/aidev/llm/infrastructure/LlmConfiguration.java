7503effbc85b2b5ace2169fc2419eb405197c22d
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 19:49:19 -0300

    feat: enable llm pricing configuration

--- src/main/java/com/ordevia/aidev/llm/infrastructure/LlmConfiguration.java
+++ src/main/java/com/ordevia/aidev/llm/infrastructure/LlmConfiguration.java
@@ -4,5 +4,5 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(LlmRoutingProperties.class)
+@EnableConfigurationProperties({LlmRoutingProperties.class,LlmPricingProperties.class})
 public class LlmConfiguration {}
