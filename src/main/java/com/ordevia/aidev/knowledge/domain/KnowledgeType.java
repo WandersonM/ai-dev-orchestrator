f8028b3eaaa428d7eed08effbcb8f9314fde617b
Wanderson Martins <martins.wanderson@hotmail.com>
2026-08-31 20:18:40 -0300

    feat: define project knowledge types

---
+++ src/main/java/com/ordevia/aidev/knowledge/domain/KnowledgeType.java
@@ -0,0 +1,11 @@
+package com.ordevia.aidev.knowledge.domain;
+
+public enum KnowledgeType {
+    FACT,
+    BUSINESS_RULE,
+    DECISION,
+    CONSTRAINT,
+    INTEGRATION_RULE,
+    ARCHITECTURE_DECISION,
+    KNOWN_LEGACY_BEHAVIOR
+}
